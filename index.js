557e64e1bc817f2dc48a549f682c4722cb6fd09a
Shaked <sakee121@gmail.com>
2022-04-06 23:17:57 +0300

    logics row must be equals to solution to continue to the next row

--- index.js
+++ index.js
@@ -2,26 +2,74 @@ var col_len_num = 5;
 var rows_len = 2;
 var row_index = 0;
 var col_index = 0;
+var isWinner = false;
+var arr = ['3','*','1','-','9'];
+var solution = -6;
 
-function get_random_number() {
-  let random = Math.floor(Math.random()*20-10);
-  //window.alert(random + " is the chosen number");
-  return random;
+// function get_random_number() {
+//   var count = 0;
+//   var str;
+//   var string = "";
+//   var value;
+//   var random;
+//   while(count < col_len_num) {
+//     value = Math.floor(Math.random()*14);
+//     if(value == 10) str = '+';
+//     else if(value == 11) str = '-';
+//     else if(value == 12) str = '*';
+//     else if(value == 13) str = '/';
+//     else str = String(value);
+
+//     if(count == col_len_num-1 && (str == '*' || str == '/' || str == '+' || str == '-'))
+//       continue;
+//     if(count == 0 && (str == '*' || str == '/'))
+//       continue;
+//     if(count > 0 && arr[count-1] == count && (str == '*' || str == '/' || str == '+' || str == '-'))
+//       continue;
+//     arr[count] = str;
+//     string+=str;
+//     count++;
+//   }
+//     random = Math.floor(eval(string));
+//     console.log(random);
+  
+//   return random;
+// }
+
+
+function checkRowEqualSolution(target) {
+  let val;
+  let value = "";
+  
+  for(var i = 0; i< col_len_num; i++) {
+    _id = row_index * 10 + i;
+    val = document.getElementById(_id).textContent;
+    console.log(val)
+    value += String(val);
+  }
+  
+  try {
+    //console.log(eval(value) + " " + value + " " + target)
+    //console.log("what")
+    if (eval(value) == target)
+      return true;
+  }
+  catch(e){console.log(e)}
+  return false;
 }
 
 
 
 function update_button(element) {
   const val = element.textContent;
-  // delete option
+  //console.log(col_index + " " + row_index)
   if (String(val) == "⌫") {
     if (col_index == 0 && row_index == 0) 
       return;
+    if(col_index == 0)
+    return;
     col_index--;
-    if(col_index < 0) {
-      col_index = col_len_num-1;
-      row_index--;
-    }
+
     if (row_index < 0) {
       row_index = 7;
       row_index = 0;
@@ -32,12 +80,24 @@ function update_button(element) {
   else {
     _id = row_index * 10 + col_index;
     if (row_index == rows_len) return;
-    col_index++;
-    document.getElementById(_id).innerHTML = val;
-    if (col_index == col_len_num) {
-      col_index = 0;
-      row_index++;
+    
+    
+    
+    if (col_index == col_len_num -1) {
+      document.getElementById(_id).innerHTML = val;
+      if(checkRowEqualSolution(solution) == true) {
+        col_index = 0;
+        row_index++;
+      }
+      else {
+        col_index = col_len_num;
+      }
     }
+    else if(col_index < col_len_num -1) {
+      document.getElementById(_id).innerHTML = val;
+      col_index++;
+    }
+          
   }
 }
 
@@ -58,7 +118,8 @@ function create_calculator() {
         className: "num-row btn-calc",
         innerHTML: values[i][j],
         onclick: function () {
-          update_button(this);
+          if(!isWinner)
+            update_button(this);
         },
       });
       row.appendChild(btn);
@@ -68,7 +129,9 @@ function create_calculator() {
 }
 
 function create_num_matrix() {
-  let random = get_random_number();
+  let random = solution;
+  //let random = Math.floor(Math.random()*20-10);
+  //window.alert(random + " is the chosen number");
   var matrix = Object.assign(document.createElement("div"), {
     className: "box rows",
   });
